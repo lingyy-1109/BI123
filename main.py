e2833827b71f62e2ec5ffbcb48d5fcdcea05d4e7
lingyy-1109 <lingyy@sjtu.edu.cn>
2022-11-14 13:03:54 +0800

    20221114 Lab2
    更新了lab2的相关内容

--- main.py
+++ main.py
@@ -4,12 +4,10 @@ from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
 import sys
 import numpy as np
-from PIL import Image
 import cv2
-import matplotlib
 import pyqtgraph as pg
-import pyqtgraph.opengl as gl
 import math
+from scipy import signal
 
 class Window(Ui_MainWindow, QMainWindow):
     def __init__(self):
@@ -22,8 +20,9 @@ class Window(Ui_MainWindow, QMainWindow):
         self.actionImport_bmp.triggered.connect(self.open_bmp)
         self.OtsuButton.clicked.connect(self.Otsu)
         self.EntropyButton.clicked.connect(self.Entropy)
-
-
+        self.ConfirmButton.clicked.connect(self.Confirm)
+        self.SobelButton.clicked.connect(self.SobelFilter)
+        self.MedianButton.clicked.connect(self.MedianFilter)
 
 
     def initHist(self):
@@ -45,6 +44,7 @@ class Window(Ui_MainWindow, QMainWindow):
         self.pwPro.addItem(self.hLine, ignoreBounds=True)
         proxy2 = pg.SignalProxy(self.pwPro.scene().sigMouseMoved, rateLimit=60, slot=self.mouseMovedPro)  # 鼠标移动获取位置
 
+    # 图像+直方图绘制
     def mouseMovedOri(self, evt):
         pos = evt[0]
         if self.pwOri.sceneBoundingRect().contains(pos):
@@ -64,14 +64,12 @@ class Window(Ui_MainWindow, QMainWindow):
             self.hLine.setPos(mousePoint.y())
 
     def drawHist(self, img, pw):  # 绘制图像直方图
+        # 清空画布
+        pw.clear()
         # 显示数据
-        rows, cols = img.shape[:2]
-
         if img.dtype == 'uint8':
-            deep = '8位'
             weight = 256
         elif img.dtype == 'uint16':
-            deep = '16位'
             weight = 65536
 
         x = np.linspace(0.5, weight - 0.5, weight)
@@ -92,34 +90,13 @@ class Window(Ui_MainWindow, QMainWindow):
             pw.plot(x, hist_g, stepMode="center", pen=penstate_g)
             pw.plot(x, hist_r, stepMode="center", pen=penstate_r)
 
-            # self.label_imgdeep.setText(str(deep))
-            minVal, maxVal, _, _ = cv2.minMaxLoc(img[:, :, 0])
-            # self.label_max.setText(str(int(maxVal)))
-            # self.label_min.setText(str(int(minVal)))
-            mean, std = cv2.meanStdDev(img[:, :, 0])
-            mean = round(mean[0][0], 3)
-            std = round(std[0][0], 3)
-            # self.label_mean.setText(str(mean))
-            # self.label_stddev.setText(str(std))
         else:
-            # self.label_imgcolor.setText("黑白")
-
             hist = cv2.calcHist([img], [0], None, [weight], [0, weight])
             hist_b = np.ravel(hist[1:])
 
             penstate = pg.mkPen(width=2, color='c')  # 绘图格式
 
             pw.plot(x, hist_b, stepMode="center", pen=penstate)
-
-            # self.label_imgdeep.setText(str(deep))
-            minVal, maxVal, _, _ = cv2.minMaxLoc(img)
-            # self.label_max.setText(str(int(maxVal)))
-            # self.label_min.setText(str(int(minVal)))
-            mean, std = cv2.meanStdDev(img)
-            mean = round(mean[0][0], 3)
-            std = round(std[0][0], 3)
-            # self.label_mean.setText(str(mean))
-            # self.label_stddev.setText(str(std))
 
     def print_img(self, image, show_label, hist_pw):
         # 在图像模块显示原图 分彩色图像和黑白图像讨论
@@ -137,6 +114,8 @@ class Window(Ui_MainWindow, QMainWindow):
         # 绘制灰度直方图
         self.drawHist(image, hist_pw)
 
+
+    # 打开、保存图像
     def open_jpg(self):
         filePath, filetype = QFileDialog.getOpenFileName(self, "选取图像", "./", "JPG Image(*.jpg)")
         self.originalImg = cv2.imread(filePath)
@@ -147,6 +126,13 @@ class Window(Ui_MainWindow, QMainWindow):
         self.originalImg = cv2.imread(filePath)
         self.print_img(self.originalImg, self.OriImg, self.pwOri)
 
+    # 确认操作(将处理后的图像保存为处理前的图像)
+    def Confirm(self):
+        if (self.originalImg is not None and self.processedImg is not None):
+            self.originalImg = self.processedImg
+            self.print_img(self.originalImg, self.OriImg, self.pwOri)
+
+    # Lab1:图像分割
     def Otsu(self):
         if(self.originalImg is not None):
             # 首先应当存在初始图像
@@ -195,27 +181,61 @@ class Window(Ui_MainWindow, QMainWindow):
             best_k = 0  # 最佳阈值
             hist = cv2.calcHist([gray], [0], None, [256], [0, 256])  # 255*1的灰度直方图的数组
             for i in range(256):
-                p.insert(i, hist[i][0] /    092.size)
+                p.insert(i, hist[i][0] / gray.size)
             for k in range(256):
                 H_b = 0  # black的熵，前景的平均信息量
                 H_w = 0  # white的熵，背景的平均信息量
                 for i in range(k):
                     if p[i] != 0:
-                        H_b = H_b - p[i] * math.log(2, p[i])
+                        H_b = H_b - p[i] * math.log(10, p[i])
 
                 for i in range(k, 256):
                     if p[i] != 0:
-                        H_w = H_w - p[i] * math.log(2, p[i])
+                        H_w = H_w - p[i] * math.log(10, p[i])
 
                 H = H_b + H_w
                 if H > H_last:
                     H_last = H
                     best_k = k
-            print(best_k)
+            # print(best_k)
             ret, self.processedImg = cv2.threshold(gray, best_k, 255, cv2.THRESH_BINARY)
-            self.processedImg = gray
+            # self.processedImg = gray
             self.print_img(self.processedImg, self.ProImg, self.pwPro)
 
+    # Lab2:图像滤波
+    def SobelFilter(self):
+        if(self.originalImg is not None):
+            # 首先应当存在初始图像
+            if(len(self.originalImg.shape) == 3):
+                gray = cv2.cvtColor(self.originalImg, cv2.COLOR_BGR2GRAY)
+            else:
+                gray = self.originalImg
+
+            filter = np.array([
+                [-1, -2, -1],
+                [0, 0, 0],
+                [1, 2, 1]
+            ])
+            self.processedImg = signal.convolve2d(gray, filter, mode='same', boundary='symm').astype(gray.dtype)
+            print(self.processedImg.dtype)
+            self.print_img(self.processedImg, self.ProImg, self.pwPro)
+
+    def MedianFilter(self):
+        if(self.originalImg is not None):
+            # 首先应当存在初始图像
+            if(len(self.originalImg.shape) == 3):
+                gray = cv2.cvtColor(self.originalImg, cv2.COLOR_BGR2GRAY)
+            else:
+                gray = self.originalImg
+
+            filter = np.array([
+                [1/9, 1/9, 1/9],
+                [1/9, 1/9, 1/9],
+                [1/9, 1/9, 1/9]
+            ])
+            self.processedImg = signal.convolve2d(gray, filter, mode='same', boundary='symm').astype(gray.dtype)
+            print(self.processedImg.dtype)
+            self.print_img(self.processedImg, self.ProImg, self.pwPro)
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
